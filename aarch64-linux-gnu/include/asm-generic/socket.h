9ca7fa04f3eb249fa7c426ba972802ece36d6259
secuflag <secuflag@ticino.com>
2021-11-01 17:41:10 +0100

    GCC 11.2.1 20211101
     * binutils-2.37
     * gmp-6.2.1
     * mpfr-4.1.0
     * mpc-1.2.1
     * isl-0.24
     * glibc-2.34
     * linux 5.15

--- aarch64-linux-gnu/include/asm-generic/socket.h
+++ aarch64-linux-gnu/include/asm-generic/socket.h
@@ -124,6 +124,8 @@
 
 #define SO_NETNS_COOKIE		71
 
+#define SO_BUF_LOCK		72
+
 
 #if __BITS_PER_LONG == 64 || (defined(__x86_64__) && defined(__ILP32__))
 /* on 64-bit and x32, avoid the ?: operator */
