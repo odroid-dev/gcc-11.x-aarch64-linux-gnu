9f82f27c2d95e75ca2828119fd5270544291922e
secuflag <secuflag@ticino.com>
2021-12-01 14:44:56 +0100

    GCC 11.2.1 20211201
     * binutils-2.37
     * gmp-6.2.1
     * mpfr-4.1.0
     * mpc-1.2.1
     * isl-0.24
     * glibc-2.34
     * linux 5.15.5

--- aarch64-linux-gnu/include/c++/11.2.1/aarch64-linux-gnu/bits/c++config.h
+++ aarch64-linux-gnu/include/c++/11.2.1/aarch64-linux-gnu/bits/c++config.h
@@ -34,7 +34,7 @@
 #define _GLIBCXX_RELEASE 11
 
 // The datestamp of the C++ library in compressed ISO date format.
-#define __GLIBCXX__ 20211101
+#define __GLIBCXX__ 20211201
 
 // Macros for various attributes.
 //   _GLIBCXX_PURE
