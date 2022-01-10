de8db739de76aaa9d8c97d4da61b119f30478571
secuflag <secuflag@ticino.com>
2022-01-10 17:05:56 +0100

    GCC 11.2.1 20220110
     * binutils-2.37
     * gmp-6.2.1
     * mpfr-4.1.0
     * mpc-1.2.1
     * isl-0.24
     * glibc-2.34
     * linux 5.16
    
    Change-Id: I62ff30701a2a5808911b1047f0b489627a6fa961

--- aarch64-linux-gnu/include/asm-generic/socket.h
+++ aarch64-linux-gnu/include/asm-generic/socket.h
@@ -126,6 +126,8 @@
 
 #define SO_BUF_LOCK		72
 
+#define SO_RESERVE_MEM		73
+
 
 #if __BITS_PER_LONG == 64 || (defined(__x86_64__) && defined(__ILP32__))
 /* on 64-bit and x32, avoid the ?: operator */
