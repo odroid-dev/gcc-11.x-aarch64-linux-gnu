23a41113c8ab4e9a4e4f31626a02c5640cefc628
secuflag <secuflag@ticino.com>
2022-03-24 15:09:29 +0100

    GCC 11.2.1 20220324
     * binutils-2.38
     * gmp-6.2.1
     * mpfr-4.1.0
     * mpc-1.2.1
     * isl-0.24
     * glibc-2.35
     * linux 5.17

--- aarch64-linux-gnu/include/asm/hwcap.h
+++ aarch64-linux-gnu/include/asm/hwcap.h
@@ -76,5 +76,7 @@
 #define HWCAP2_BTI		(1 << 17)
 #define HWCAP2_MTE		(1 << 18)
 #define HWCAP2_ECV		(1 << 19)
+#define HWCAP2_AFP		(1 << 20)
+#define HWCAP2_RPRES		(1 << 21)
 
 #endif /* __ASM_HWCAP_H */
